6ce2b3ba3959945c5e34012de389a5fd37616b4f
Lily326-ux <uprt.lily228@gmail.com>
2021-07-20 21:21:46 +0300

    get_currency

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,12 +1,9 @@
 from requests import get
 
 def make_get_request(url):
-    request_result = get(url=url)
-    print(request_result)
+    request_result = get(url)
     if (request_result.status_code == 200):
         return request_result.json()
     else:
         raise Exception()
 
-def return_get_string():
-    return f'https://newsapi.org/v2/everything?q=tesla&from=2021-06-20&sortBy=publishedAt&apiKey=8836e4490fcf4387b55ae5bac665210b'
